d2a2ddf8064c2e7e95c1235d4fba5c5b36c6c240
Jae Huh <jaeeunhuh@gmail.com>
2017-04-26 23:30:06 +1200

    Set up for test

--- bowling/game.js
+++ bowling/game.js
@@ -1,3 +1,10 @@
+module.export = {
+  addOpenFrameScore,
+  addSpareScore,
+  addStrikeScore,
+  calculateScore
+}
+
 // Score 119:
 // var frames = [
 //   [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]
---
+++ bowling/game.test.js
@@ -0,0 +1,20 @@
+var test = require('tape')
+
+var game = require('game')
+
+// test('Test test test', function(t) {
+//   t.pass()
+//   t.end()
+// })
+
+test('game.addOpenFrameScore adds single scores', function (t) {
+  // Arrange
+  var score = [[1, 8], [2, 7], [3, 5]]
+  var expected = 26
+
+  // Act
+  var actual = game.addOpenFrameScore(score, '26')
+
+  // Assert
+  t.deepEqual(actual, expected)
+})
