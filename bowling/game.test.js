ce3ab28f063993e5b6639551dcbcf440b5ed8a77
Jae Huh <jaeeunhuh@gmail.com>
2017-04-27 13:42:18 +1200

    Write tests for each functions

--- bowling/game.test.js
+++ bowling/game.test.js
@@ -19,3 +19,27 @@ test('game.addOpenFrameScore adds single scores', function (t) {
   t.equals(actual, expected)
   t.end()
 })
+
+test('game.addSpareScore adds scores of spared frames', function (t) {
+  var score = [
+    [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
+  ]
+  var expected = 15
+
+  var actual = game.addSpareScore(score)
+
+  t.equals(actual, expected)
+  t.end()
+})
+
+test('game.addStrikeScore adds scores for strikes', function (t) {
+  var score = [
+    [1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [10, 10, 10]
+  ]
+  var expected = 91
+
+  var actual = game.addStrikeScore(score)
+
+  t.equals(actual, expected)
+  t.end()
+})
