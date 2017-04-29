26393985b400bd9b814c0f41fa1956190cb9e15b
Jae Huh <jaeeunhuh@gmail.com>
2017-04-29 21:28:10 +1200

    Post data to the server from HTML form

--- express-server/index.js
+++ express-server/index.js
@@ -1,16 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const server = require('./server')
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.listen(PORT, function () {
+  console.log('server listening on port: ', PORT)
+})
+
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', server.compliment)
 app.get('/profile', server.profile)
 app.get('/profile/:id', server.profiles)
-
-app.listen(PORT, function () {
-  console.log('server listening on port: ', PORT)
-})
+app.get('/get-name', server.getName)
+app.post('/named-compliment', server.namedCompliment)
--- express-server/server.js
+++ express-server/server.js
@@ -13,16 +13,29 @@ function profiles (req, res) {
   const id = req.params.id
   if (id === '1') {
     res.sendFile(path.join(__dirname, 'public', 'silvia.html'))
-  } else if (id === '2') {
-    res.sendFile(path.join(__dirname, 'public', 'sampson.html'))
-  } else {
-    res.status(404).send("404")
   }
+  if (id === '2') {
+    res.sendFile(path.join(__dirname, 'public', 'sampson.html'))
+  }
+  // else {
+  //   res.status(404).send("404")
+  // }
+}
+
+function getName(req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'get-name.html'))
+}
+
+function namedCompliment(req, res) {
+  const name = req.body.name
+  res.send(`<h1>${name}, you are amazing!</h1>`)
 }
 
 
 module.exports = {
   compliment,
   profile,
-  profiles
+  profiles,
+  getName,
+  namedCompliment
 }
