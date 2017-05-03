e4809f2fe25c3182d97b31ee936db1686a7a25df
Jae Huh <jaeeunhuh@gmail.com>
2017-05-03 20:47:23 +1200

    User story 2

--- pupparazzi/routes.js
+++ pupparazzi/routes.js
@@ -1,4 +1,41 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
 
 module.exports = router
+
+router.get('/', (req, res) => {
+  res.redirect('/puppies')
+})
+
+function getFileContents (file, callback) {
+  fs.readFile(file, 'utf-8', (err, contents) => {
+    if (err) {
+      callback(err)
+    }
+    const data = JSON.parse(contents)
+    callback(null, data)
+  })
+}
+
+router.get('/puppies', (req, res) => {
+  getFileContents('./data.json', (err, contents) => {
+    if (err) {
+      return err
+    }
+    res.render('./puppies/index', contents)
+  })
+})
+
+router.get('/puppies/:id', (req, res) => {
+  const id = req.params.id
+  getFileContents('./data.json', (err, contents) => {
+    if (err) {
+      return err
+    }
+    var puppy = contents.puppies.find((puppy) => {
+      return puppy.id === Number(id)
+    })
+    res.render('./puppies/view', puppy)
+  })
+})
