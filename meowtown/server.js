f33436ebd401b2202d06a752c7ca7a27291c58ed
Jae Huh <jaeeunhuh@gmail.com>
2017-05-04 21:22:45 +1200

    Release 6 completed

--- meowtown/server.js
+++ meowtown/server.js
@@ -24,8 +24,10 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 var data = {
   cats: [
-    {id: 1, name: 'fluffy'},
-    {id: 2, name: 'tick'}
+    {id: 1, name: 'fluffy', age: 3},
+    {id: 2, name: 'tick', age: 8},
+    {id: 3, name: 'ember', age: 14},
+    {id: 4, name: 'bo', age: 12}
   ]
 }
 
@@ -45,10 +47,18 @@ server.get('/cats/new', function (req, res) {
 
 server.get('/cats/:id', function (req, res) {
   console.log(req.params) // try going to /cats/1
+  res.render('show', data.cats[req.params.id-1])
 })
 
 server.post('/cats', function (req, res) {
   console.log(req.body)
+  data.cats.push(req.body)
+  res.redirect('/cats')
+})
+
+server.get('/cats/edit/:id', (req, res) => {
+  const id = req.params.id
+  res.render('edit', data.cats[id-1])
 })
 
 module.exports = server
